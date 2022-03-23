ff586a872c324b9ee0c664923b5cff79939ad590
flamechain <rwc@thezulus.com>
2022-03-23 10:27:01 -0700

    prints unix-style parse-tree

--- src/Buckle/Silver/compiler.cs
+++ src/Buckle/Silver/compiler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Buckle {
@@ -47,7 +48,7 @@ namespace Buckle {
         }
     }
 
-    enum TokenType {
+    enum SyntaxType {
         Invalid,
         EOF,
         NUMBER,
@@ -58,20 +59,59 @@ namespace Buckle {
         SOLIDUS,
         LPAREN,
         RPAREN,
+        NUMBER_EXPR,
+        BINARY_EXPR,
+        UNARY_EXPR,
+        PAREN_EXPR,
     }
 
-    class Token {
-        public TokenType type { get; }
+    abstract class Node {
+        public abstract SyntaxType type { get; }
+        public abstract List<Node> GetChildren();
+    }
+
+    class Token : Node {
+        public override SyntaxType type { get; }
         public int pos { get; }
         public string text { get; }
         public object value { get; }
 
-        public Token(TokenType type_, int pos_, string text_, object value_) {
+        public Token(SyntaxType type_, int pos_, string text_, object value_) {
             type = type_;
             pos = pos_;
             text = text_;
             value = value_;
         }
+
+        public override List<Node> GetChildren() { return new List<Node>() { }; }
+    }
+
+    abstract class Expression : Node { }
+
+    class NumberNode : Expression {
+        public Token token { get; }
+        public override SyntaxType type => SyntaxType.NUMBER_EXPR;
+
+        public NumberNode(Token token_) {
+            token = token_;
+        }
+
+        public override List<Node> GetChildren() { return new List<Node>() { token }; }
+    }
+
+    class BinaryExpression : Expression {
+        public Expression left { get; }
+        public Token op { get; }
+        public Expression right { get; }
+        public override SyntaxType type => SyntaxType.BINARY_EXPR;
+
+        public BinaryExpression(Expression left_, Token op_, Expression right_) {
+            left = left_;
+            op = op_;
+            right = right_;
+        }
+
+        public override List<Node> GetChildren() { return new List<Node>() { left, op, right }; }
     }
 
     class Lexer {
@@ -92,7 +132,7 @@ namespace Buckle {
         private void Advance() { pos_++; }
 
         public Token Next() {
-            if (pos_ >= text_.Length) return new Token(TokenType.EOF, pos_, "\0", null);
+            if (pos_ >= text_.Length) return new Token(SyntaxType.EOF, pos_, "\0", null);
 
             if (char.IsDigit(current)) {
                 int start = pos_;
@@ -103,7 +143,7 @@ namespace Buckle {
                 int length = pos_ - start;
                 string text = text_.Substring(start, length);
                 int.TryParse(text, out int value);
-                return new Token(TokenType.NUMBER, start, text, value);
+                return new Token(SyntaxType.NUMBER, start, text, value);
             } else if (char.IsWhiteSpace(current)) {
                 int start = pos_;
 
@@ -112,16 +152,72 @@ namespace Buckle {
 
                 int length = pos_ - start;
                 string text = text_.Substring(start, length);
-                return new Token(TokenType.WHITESPACE, start, text, null);
-            } else if (current == '+') return new Token(TokenType.PLUS, pos_++, "+", null);
-            else if (current == '-') return new Token(TokenType.MINUS, pos_++, "-", null);
-            else if (current == '*') return new Token(TokenType.ASTERISK, pos_++, "*", null);
-            else if (current == '/') return new Token(TokenType.SOLIDUS, pos_++, "/", null);
-            else if (current == '(') return new Token(TokenType.LPAREN, pos_++, "(", null);
-            else if (current == ')') return new Token(TokenType.RPAREN, pos_++, ")", null);
+                return new Token(SyntaxType.WHITESPACE, start, text, null);
+            } else if (current == '+') return new Token(SyntaxType.PLUS, pos_++, "+", null);
+            else if (current == '-') return new Token(SyntaxType.MINUS, pos_++, "-", null);
+            else if (current == '*') return new Token(SyntaxType.ASTERISK, pos_++, "*", null);
+            else if (current == '/') return new Token(SyntaxType.SOLIDUS, pos_++, "/", null);
+            else if (current == '(') return new Token(SyntaxType.LPAREN, pos_++, "(", null);
+            else if (current == ')') return new Token(SyntaxType.RPAREN, pos_++, ")", null);
 
-            return new Token(TokenType.Invalid, pos_++, text_.Substring(pos_-1,1), null);
+            return new Token(SyntaxType.Invalid, pos_++, text_.Substring(pos_-1,1), null);
         }
+    }
+
+    class Parser {
+        private readonly Token[] tokens_;
+        private int pos_;
+
+        public Parser(string text) {
+            var tokens = new List<Token>();
+            Lexer lexer = new Lexer(text);
+            Token token;
+
+            do {
+                token = lexer.Next();
+
+                if (token.type != SyntaxType.WHITESPACE && token.type != SyntaxType.Invalid)
+                    tokens.Add(token);
+            } while (token.type != SyntaxType.EOF);
+
+            tokens_ = tokens.ToArray();
+        }
+
+        public Expression Parse() {
+            var left = ParsePrimaryExpression();
+
+            while (current.type == SyntaxType.PLUS || current.type == SyntaxType.MINUS) {
+                var op = Next();
+                var right = ParsePrimaryExpression();
+                left = new BinaryExpression(left, op, right);
+            }
+
+            return left;
+        }
+
+        private Expression ParsePrimaryExpression() {
+            var token = Match(SyntaxType.NUMBER);
+            return new NumberNode(token);
+        }
+
+        private Token Match(SyntaxType type) {
+            if (current.type == type) return Next();
+            return new Token(type, current.pos, null, null);
+        }
+
+        private Token Next() {
+            Token cur = current;
+            pos_++;
+            return cur;
+        }
+
+        private Token Peek(int offset) {
+            int index = pos_ + offset;
+            if (index >= tokens_.Length) return tokens_[tokens_.Length-1];
+            return tokens_[index];
+        }
+
+        private Token current => Peek(0);
     }
 
     public class Compiler {
@@ -156,6 +252,21 @@ namespace Buckle {
             diagnostics.Add(new Diagnostic(DiagnosticType.warning, "preprocessing not supported (yet); skipping"));
         }
 
+        private void PrettyPrint(Node node, string indent = "", bool islast=true) {
+            string marker = islast ? "└─" : "├─";
+            Console.Write($"{indent}{marker}{node.type}");
+
+            if (node is Token t && t.value != null)
+                Console.Write($" {t.value}");
+
+            Console.WriteLine();
+            indent += islast ? "  " : "│ ";
+            var lastChild = node.GetChildren().LastOrDefault();
+
+            foreach (var child in node.GetChildren())
+                PrettyPrint(child, indent, child == lastChild);
+        }
+
         private void Repl() {
             state.link_output_content = null;
 
@@ -163,16 +274,15 @@ namespace Buckle {
                 Console.Write("> ");
                 string line = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) return;
-                Lexer lexer = new Lexer(line);
+                Parser parser = new Parser(line);
+                var expr = parser.Parse();
 
-                while (true) {
-                    Token token = lexer.Next();
-                    if (token.type == TokenType.EOF) break;
-                    Console.Write($"{token.type}: '{token.text}'");
-                    if (token.value != null) Console.Write($" {token.value}");
-                    Console.WriteLine();
-                }
+                ConsoleColor prev = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGray;
 
+                PrettyPrint(expr);
+
+                Console.ForegroundColor = prev;
             }
         }
 
