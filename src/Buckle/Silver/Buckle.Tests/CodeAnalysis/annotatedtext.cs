f3e59b74d6cf4dbd7e1ad0500d99c982f9ca64ab
Wilson_Ryan <wilsr394@hsd.k12.or.us>
2022-03-30 11:15:24 -0700

    unpushed work

---
+++ src/Buckle/Silver/Buckle.Tests/CodeAnalysis/annotatedtext.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Buckle.CodeAnalysis.Text;
+
+namespace Buckle.Tests.CodeAnalysis {
+
+    internal sealed class AnnotatedText {
+        public string text { get; }
+        public ImmutableArray<TextSpan> spans { get; }
+
+        private AnnotatedText(string text_, ImmutableArray<TextSpan> spans_) {
+            text = text_;
+            spans = spans_;
+        }
+
+        public static AnnotatedText Parse(string text) {
+            text = Unindent(text);
+
+            var textBuilder = new StringBuilder();
+            var spanBuilder = ImmutableArray.CreateBuilder<TextSpan>();
+            var startStack = new Stack<int>();
+
+            var pos = 0;
+
+            foreach (var c in text) {
+                if (c == '[') {
+                    startStack.Push(pos);
+                } else if (c == ']') {
+                    if (startStack.Count == 0)
+                        throw new ArgumentException("']' without corresponding '[' in text", nameof(text));
+
+                    var start = startStack.Pop();
+                    var end = pos;
+                    var span = TextSpan.FromBounds(start, end);
+                    spanBuilder.Add(span);
+                } else {
+                    pos++;
+                    textBuilder.Append(c);
+                }
+            }
+
+            if (startStack.Count != 0)
+                throw new ArgumentException("'[' without corresponding ']' in text", nameof(text));
+
+            return new AnnotatedText(textBuilder.ToString(), spanBuilder.ToImmutable());
+        }
+
+        private static string Unindent(string text) {
+            var lines = UnindentLines(text);
+            return string.Join(Environment.NewLine, lines);
+        }
+
+        public static string[] UnindentLines(string text) {
+            var lines = new List<string>();
+
+            using (var stringReader = new StringReader(text)) {
+                string line;
+
+                while ((line = stringReader.ReadLine()) != null)
+                    lines.Add(line);
+            }
+
+            var minIndent = int.MaxValue;
+            for (int i=0; i<lines.Count; i++) {
+                var line = lines[i];
+
+                if (line.Trim().Length == 0) {
+                    lines[i] = string.Empty;
+                    continue;
+                }
+
+                var indent = line.Length - line.TrimStart().Length;
+                minIndent = Math.Min(minIndent, indent);
+            }
+
+            for (var i=0; i<lines.Count; i++) {
+                if (lines[i].Length == 0)
+                    continue;
+
+                lines[i] = lines[i].Substring(minIndent);
+            }
+
+            while (lines.Count > 0 && lines[0].Length == 0)
+                lines.RemoveAt(0);
+
+            while (lines.Count > 0 && lines[lines.Count - 1].Length == 0)
+                lines.RemoveAt(lines.Count - 1);
+
+            return lines.ToArray();
+        }
+    }
+}
--- src/Buckle/Silver/Buckle.Tests/CodeAnalysis/evaluator_test.cs
+++ src/Buckle/Silver/Buckle.Tests/CodeAnalysis/evaluator_test.cs
@@ -37,5 +37,101 @@ namespace Buckle.Tests.CodeAnalysis {
             Assert.Empty(result.diagnostics.ToArray());
             Assert.Equal(expectedValue, result.value);
         }
+
+        [Fact]
+        public void Evaluator_VariableDelcaration_Reports_Reclaration() {
+            var text = @"
+                auto x = 10;
+                auto y = 100;
+                {
+                    auto x = 10;
+                }
+                auto [x] = 5;
+            ";
+
+            var diagnostics = @"
+                redefinition of 'x'
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Name_Reports_Undefined() {
+            var text = @"[x] * 10;";
+
+            var diagnostics = @"
+                undefined symbol 'x'
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigns_Reports_Undefined() {
+            var text = @"[x] = 10;";
+
+            var diagnostics = @"
+                undefined symbol 'x'
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigns_Reports_Readonly() {
+            var text = @"
+                let x = 10;
+                x [=] 0;
+            ";
+
+            var diagnostics = @"
+                assignment of read-only variable 'x'
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_Assigns_Reports_CannotConvert() {
+            var text = @"
+                auto x = 10;
+                x = [false];
+            ";
+
+            var diagnostics = @"
+                cannot convert from System.Boolean to System.Int32
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        private void AssertDiagnostics(string text, string diagnosticText) {
+            var annotatedText = AnnotatedText.Parse(text);
+            var syntaxTree = SyntaxTree.Parse(annotatedText.text);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
+            var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
+
+            if (annotatedText.spans.Length != expectedDiagnostics.Length)
+                throw new Exception("must mark as many spans as there are diagnostics");
+
+            Assert.Equal(expectedDiagnostics.Length, result.diagnostics.count);
+
+            for (int i=0; i<expectedDiagnostics.Length; i++) {
+                var diagnostic = result.diagnostics.Pop();
+
+                var expectedMessage = expectedDiagnostics[i];
+                var actualMessage = diagnostic.msg;
+                Assert.Equal(expectedMessage, actualMessage);
+
+                var expectedSpan = annotatedText.spans[i];
+                var actualSpan = diagnostic.span;
+                Assert.Equal(expectedSpan.start, actualSpan.start);
+                Assert.Equal(expectedSpan.end, actualSpan.end);
+                Assert.Equal(expectedSpan.length, actualSpan.length);
+            }
+        }
     }
 }
