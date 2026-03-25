316f9fdb0a130ef88d3cfc7547fa2d16b8f5f643
Ryan Wilson <rwc@thezulus.com>
2026-03-25 15:32:08 -0500

    embedded native dlls (#112)

--- src/Buckle/CommandLine/Program.cs
+++ src/Buckle/CommandLine/Program.cs
@@ -2,6 +2,9 @@ using CommandLine;
 
 public static class Program {
     public static int Main(string[] args) {
+#if !DEBUG
+        DllImportHelper.ExtractAndLoadDlls();
+#endif
         return BuckleCommandLine.ProcessArgs(args);
     }
 }
